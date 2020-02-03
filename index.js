cfd634a905c2049f4f961c861fdc9d8feba3802f
dionaditya <dionjatingaleh@gmail.com>
2020-02-03 21:55:49 +0700

    add redis

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const models = require("./models");
+const models = require("./server/models");
 const bodyParser = require("body-parser");
 const expressGraphql = require("express-graphql");
 const mongoose = require("mongoose");
@@ -7,17 +7,20 @@ const cors = require("cors");
 const { buildSchema } = require("graphql");
 const bcrypt = require("bcrypt");
 const JWT = require("jsonwebtoken");
-const jwt = require('express-jwt')
+const jwt = require("express-jwt");
 const User = mongoose.model("user");
+const Product = mongoose.model("product");
+const redisClient = require("redis").createClient;
+const redisReady=  redisClient(6379, "localhost");
 
-const ck = require('ckey');
+const ck = require("ckey");
 
 // Construct a schema, using GraphQL schema language
 
 const auth = jwt({
   secret: ck.JWT_KEY,
-  credentialsRequired: true
-})
+  credentialsRequired: false
+});
 
 const schema = buildSchema(`
   type User {
@@ -27,8 +30,16 @@ const schema = buildSchema(`
     password: String
   }
 
+  type Product {
+    id: ID,
+    title: String,
+    category: String
+  }
+
   type Query {
-    me: User
+    user: User,
+    products: [Product]
+    product(id: String!): Product
   }
 
   type Mutation {
@@ -39,7 +50,7 @@ const schema = buildSchema(`
 
 const app = express();
 
-const MONGO_URL = ck.DB_URL 
+const MONGO_URL = ck.DB_URL;
 
 mongoose.Promise = global.Promise;
 
@@ -52,15 +63,52 @@ mongoose.connection
   .once("open", () => console.log("connected to mongodb"))
   .on("error", error => console.log("Erorr connection"));
 
+redisReady.on("connect", () => {
+  console.log("connected to Redis");
+});
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const root =  {
-  me: async (_, args, context) => {
-    const result = await User.findById(args.user.id).exec()
+const root = {
+  user: async (_, args, context) => {
+    const result = await User.findById(args.user.id).exec();
     // user is authenticated
-    return result
+    return result;
+  },
+  products: async (_, args, content) => {
+    return new Promise((resolve, reject) => {
+      resolve(Product
+            .find()
+            .exec())
+    });
+  },
+  product: async ({id}, args) => {
+    const getProduct = new Promise((resolve, reject) => {
+      args.redisReady.get(id, async (err, reply) => {
+        if(err) {
+          throw new Error ('error')
+        } else if(reply) {
+          const parseReply = JSON.parse(reply)
+          const replyProduct = {
+            id: parseReply._id,
+            title: parseReply.title,
+            category: parseReply.category
+          }
+          return resolve(replyProduct)
+        } else {
+          return Product
+            .findOne({_id: id})
+            .exec()
+            .then((product) => {
+              console.log('product', product)
+              args.redisReady.set(id, JSON.stringify(product))
+              resolve(product)
+            })
+        }
+      })
+    });
+    return await getProduct
   },
   signUp: async ({ username, email, password }) => {
     const user = await new User({
@@ -74,30 +122,26 @@ const root =  {
       ck.JWT_KEY || "somscret",
       { expiresIn: "1y" }
     );
-
   },
-  login: async ({email, password}) => {
-    console.log(email)
-    const user = await User.findOne({email: email }).exec()
+  login: async ({ email, password }) => {
+    console.log(email);
+    const user = await User.findOne({ email: email }).exec();
 
-    if(!user) {
-      throw new Error('No user with this email');
+    if (!user) {
+      throw new Error("No user with this email");
     }
 
-    const valid = await bcrypt.compare(password, user.password)
-    
+    const valid = await bcrypt.compare(password, user.password);
+
     if (!valid) {
-      throw new Error('Incorrect password')
+      throw new Error("Incorrect password");
     }
 
-    return JWT.sign(
-      { id: user.id, email: user.email },
-      ck.JWT_KEY,
-      { expiresIn: '1d' }
-    )
+    return JWT.sign({ id: user.id, email: user.email }, ck.JWT_KEY, {
+      expiresIn: "1d"
+    });
   }
 };
-
 
 app.use(
   "/graphql",
@@ -105,12 +149,13 @@ app.use(
   expressGraphql(req => {
     return {
       context: {
-        user: req.user
+        user: req.user,
+        redisReady
       },
       schema: schema,
       rootValue: root,
-      graphiql: true,
-    }
+      graphiql: true
+    };
   })
 );
 
